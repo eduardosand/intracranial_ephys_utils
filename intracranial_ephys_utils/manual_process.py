95361a0be92078b7b8688e0d07f9bedd51fa5b14
Eduardo Sandoval <eduardosand@users.noreply.github.com>
2023-12-22 15:38:35 -0500

    added better labels to get at timestamps

--- intracranial_ephys_utils/manual_process.py
+++ intracranial_ephys_utils/manual_process.py
@@ -6,25 +6,25 @@ import os
 import pandas as pd
 
 
-def reformat_event_labels(event_folder, output_folder, subject, session, task):
+def reformat_event_labels(subject, session, task, data_directory):
     """
     This script takes the events files, reads the timestamps in, and organizes them suitably for
     the data_viewer.
-    :param event_folder: Path object, where the neuralynx data sits in
-    :param output_folder: where to put the events file after reformatting
     :param subject:
     :param session:
     :param task:
+    :param data_directory:
     :return:
     """
-    event_times, event_labels, global_start = get_event_times(event_folder, rescale=False)
-    event_times_sec, _, _ = get_event_times(event_folder, rescale=True)
-    durations = np.zeros((event_times_sec.shape[0], 1))
-    source_epoch = pd.DataFrame([event_times_sec, durations, event_labels], columns=['time', 'duration', 'label'])
-    if f'{subject}_session_{session}_{task}.csv' in os.listdir(output_folder):
+    data_dir = data_directory / subject / session
+    event_times, event_labels, global_start = get_event_times(data_dir, rescale=False)
+    event_times_sec, _, _ = get_event_times(data_dir, rescale=True)
+    durations = np.ones((event_times_sec.shape[0], ))*0.5
+    source_epoch = pd.DataFrame(np.array([event_times_sec, durations, event_labels]).T, columns=['time', 'duration', 'label'])
+    if f'{subject}_{session}_{task}.csv' in os.listdir(data_dir):
         raise FileExistsError
     else:
-        source_epoch.to_csv(output_folder / f'{subject}_session_{session}_{task}.csv', index=False)
+        source_epoch.to_csv(data_dir / f'{subject}_{session}_{task}.csv', index=False)
     return None
 
 
@@ -57,8 +57,8 @@ def photodiode_check_viewer(subject, session, task, data_directory):
     view1.auto_scale()
     win.add_view(view1)
 
-    possible_labels = ['epileptic activity']
-    filename = f'{subject}_session_{session}_{task}.csv'
+    possible_labels = [f'{task} duration']
+    filename = f'{subject}_{session}_{task}.csv'
     source_epoch = CsvEpochSource(os.path.join(data_dir, filename), possible_labels)
 
     # create a viewer for the encoder itself
@@ -79,11 +79,12 @@ def main():
     test_subject = 'IR87'
     test_session = 'sess-1'
     task = 'wcst'
-    bp = '1000'
     results_directory = Path("C:/Users/edsan/PycharmProjects/tt_su/data/")
     # task = 'Starting Recording'
     # task = 'WCST - begun'
     # task = 'wcst end'
+    # reformat_event_labels(test_subject, test_session, task, results_directory)
+
     photodiode_check_viewer(test_subject, test_session, task, results_directory)
 
     # data_clean_viewer(test_subject, test_session, task, processed_data_directory, electrode_names, dataset, eff_fs[0])
