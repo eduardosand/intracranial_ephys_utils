a9b24bcdd76423f31b3a8486191903070a297285
eduardosand <esandov7@gmail.com>
2024-05-07 15:28:58 -0700

    added function to plot time series as diagnostic (essentially allow you to remove chunks of data at a glance before parsing them through ephyviewer)

---
+++ intracranial_ephys_utils/plot_data.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def diagnostic_time_series_plot(lfp_signal, freq, total_time=None, output_folder=None, electrode_name='',
+                                subject=None, session=None):
+    """
+    Plot time series given a sampling rate to check for any weird idiosyncracies in data.
+    :param lfp_signal: (numpy array). The time series signal, ideal n_timepoints x 1.
+    :param freq: (int). The sampling rate of the signal. To get a good estimate of time
+    :param total_time: (float) (optional) Definite positive, used to limit the amount of time visualized.
+    :param output_folder: (Path) (optional) Folder where to keep data.
+    :param electrode_name: (string) (optional) Name of electrode if saving
+    :param subject:  (string) (optional) Name of subject if saving
+    :param session: (string) (optional) Name of session if saving
+    :return:
+    """
+    fig2, ax = plt.subplots(4, 1)
+    ax[0].plot(np.linspace(0, 1, num=int(freq)), lfp_signal[0:int(freq)])
+    midlevel_time = 30
+    ax[1].plot(np.linspace(0, midlevel_time, num=int(freq*midlevel_time)), lfp_signal[0:int(midlevel_time*freq)])
+    sec_in_minute = 60
+    ax[2].plot(np.linspace(0, lfp_signal.shape[0]/freq/sec_in_minute, lfp_signal.shape[0]), lfp_signal)
+    ax[3].plot(np.linspace(midlevel_time, 0, num=int(freq*midlevel_time)), lfp_signal[-int(midlevel_time*freq):])
+    for axis in ax:
+        axis.set_ylabel('Voltage (uV)')
+        axis.set_xlabel('Time (s)')
+    ax[2].set_xlabel('Time (m)')
+    if total_time is not None:
+        ax[2].set_xlim([0, total_time])
+    if electrode_name != '':
+        plt.suptitle(f'Time Courses for {electrode_name}')
+    if output_folder is not None:
+        plt.savefig(output_folder / f'{subject}_{session}_{electrode_name}.png')
+        plt.close()
+    else:
+        plt.tight_layout()
+        plt.show()
+    return None
