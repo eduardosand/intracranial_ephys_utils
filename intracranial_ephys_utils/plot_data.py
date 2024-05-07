f6a7e5736dd31b6b1786d15fbdb62d8ad7b4f4e5
eduardosand <esandov7@gmail.com>
2024-05-07 16:00:41 -0700

    added some labels for diagnostic time series plot

--- intracranial_ephys_utils/plot_data.py
+++ intracranial_ephys_utils/plot_data.py
@@ -10,18 +10,24 @@ def diagnostic_time_series_plot(lfp_signal, freq, total_time=None, output_folder
     :param freq: (int). The sampling rate of the signal. To get a good estimate of time
     :param total_time: (float) (optional) Definite positive, used to limit the amount of time visualized.
     :param output_folder: (Path) (optional) Folder where to keep data.
-    :param electrode_name: (string) (optional) Name of electrode if saving
+    :param electrode_name: (string) (optional) Name of electrode if saving, or want a title
     :param subject:  (string) (optional) Name of subject if saving
     :param session: (string) (optional) Name of session if saving
     :return:
     """
     fig2, ax = plt.subplots(4, 1)
+
     ax[0].plot(np.linspace(0, 1, num=int(freq)), lfp_signal[0:int(freq)])
+    ax[0].set_title(f'First second')
     midlevel_time = 30
     ax[1].plot(np.linspace(0, midlevel_time, num=int(freq*midlevel_time)), lfp_signal[0:int(midlevel_time*freq)])
+    ax[1].set_title(f'First {midlevel_time} seconds')
     sec_in_minute = 60
     ax[2].plot(np.linspace(0, lfp_signal.shape[0]/freq/sec_in_minute, lfp_signal.shape[0]), lfp_signal)
+    ax[2].set_title('Entire task in minutes')
     ax[3].plot(np.linspace(midlevel_time, 0, num=int(freq*midlevel_time)), lfp_signal[-int(midlevel_time*freq):])
+    ax[3].set_title(f'Last {midlevel_time} seconds')
+
     for axis in ax:
         axis.set_ylabel('Voltage (uV)')
         axis.set_xlabel('Time (s)')
