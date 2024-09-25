934868d64793f4aafadfc140a0107a262eb0f541
eduardosand <esandov7@gmail.com>
2024-09-24 23:40:34 -0700

    last bug?

--- intracranial_ephys_utils/manual_process.py
+++ intracranial_ephys_utils/manual_process.py
@@ -247,7 +247,7 @@ def su_timestamp_process(subject, session, task, data_directory, annotations_dir
             print(ext)
             photodiode_check_viewer(subject, session, task, data_directory, annotations_directory, diagnostic=False,
                                     events_filename=event_file)
-            file_root, _ = os.path.splitext(events_filename)
+            file_root, _ = os.path.splitext(event_file)
             labels_file = annotations_directory / f'{subject}_{session}_{task}_{file_root}.csv'
             task_label = labels_file[labels_file.label == f"{task} duration"]
             if len(task_label) == 0:
