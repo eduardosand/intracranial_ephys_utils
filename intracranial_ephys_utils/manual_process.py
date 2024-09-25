af22b75649c7c92e22517f679e6fa474663184f3
eduardosand <esandov7@gmail.com>
2024-09-24 23:46:41 -0700

    last bug?

--- intracranial_ephys_utils/manual_process.py
+++ intracranial_ephys_utils/manual_process.py
@@ -213,7 +213,11 @@ def write_timestamps(subject, session, task, event_folder, annotations_directory
     print(task_label['time'].iloc[0])
     start_time_sec = task_label['time'].iloc[0].astype(float)
     end_time_sec = start_time_sec + task_label['duration'].iloc[0].astype(float)
-    _, _, global_start, _ = get_event_times(event_folder, rescale=False)
+    if events_filename is None:
+        ext = None
+    else:
+        ext = events_filename[-6:]
+    _, _, global_start, _ = get_event_times(event_folder, rescale=False, extension=ext)
     # microsec_sec = 10**-6
     sec_microsec = 10**6
     start_time_machine = (start_time_sec + global_start) * sec_microsec
