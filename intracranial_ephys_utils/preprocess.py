b435009b865a33a3a5c731fe7c71ea107fa64e82
Eduardo Sandoval <esandov7@gmail.com>
2026-03-11 21:18:01 -0700

    change hyperparameter for ph diode data

--- intracranial_ephys_utils/preprocess.py
+++ intracranial_ephys_utils/preprocess.py
@@ -161,7 +161,8 @@ def binarize_ph(ph_signal, sampling_rate, task_time=None, event_threshold=2, deb
     # for IR94 session 1, signal is feeble, need to use a different thing
     events = timepoints[abs(filtered) > event_threshold * stdev]
     print(len(events))
-    buffer = 0.06*sampling_rate
+    # originally 0.06
+    buffer = 0.05*sampling_rate
     sample_size = int(0.045*sampling_rate)
     event_breakpoint = 0
     event_onsets_initial = []
